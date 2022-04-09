f9ab92f6e822c1aa96b5157b52e5ae967be1f3cc
epengc <epengc@icloud.com>
2022-04-08 20:06:15 -0700

    update txt2pcd

--- txt2pcd.cpp
+++ txt2pcd.cpp
@@ -1,57 +1,64 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include <pcl/io/pcd_io.h>
-#include "stdafx.h"
+#include <pcl/point_types.h>
+#include <pcl/visualization/pcl_visualizer.h>
 using namespace std;
 
 
-int numofPoints(char* fname){
-    int n_points = 0;
-    int c = 0;
-    FILE *fp;
-    fp = fopen(fname, "r");
-    do{
-        c = fgetc(fp);
-        if(c=='\n'){
-            ++n_points;
-        }
-    }while(c!=EOF);
-
-    fclose(fp);
-    return n_points;
-}
+typedef struct tagPOINT_3D{
+    double x;
+    double y;
+    double z;
+    double nx;
+    double ny;
+    double nz;
+}POINT_WORLD
 
 
 int main()
 {
     // the counter of points within one file.
-    int n_points = 0; 
-    FILE *fp_1;
-    fp_1 = fopen("1.xyz","r");
-    n_points = numofPoints("1.xyz");
-    cout<<"There are "<<n<<" point in the file ..."<<endl;
+    int number_txt; 
+    FILE *fp_txt;
+    tagPOINT_3D TxtPoint;
+    vector<tagPOINT_3D> m_vTxtPoints;
+    string file_name = "/home/epengc/Documents/3DPointCloudProject/*.txt";
+    fp_txt = fopen(file_name,"r");
 
-    // Create a cloud point file and put the xyz value to a points cloud pointer.
-    pcl::PointCloud<pcl::PointXYZ> cloud;
-    cloud.width = n;
-    cloud.height = 1;
-    cloud.is_dense = false;
-    cloud.points.resize(cloud.width*cloud.height);
+    if(fp_txt){
+        while(6==fscanf(fp_txt, "%lf, %lf, %lf, %lf, %lf, %lf", 
+                        &TxtPoint.x, &TxtPoint.y, &TxtPoint.z, 
+                        &TxtPoint.nx, &TxtPoint.ny, &TxtPoint.nz)){
+            m_vTxtPoints.push_back(TxtPoint);
+        }
+    }else(cout<<"Txt data is fail for loading."<<endl;)
 
-    // Put the points cloud to new points cloud pointer x, y, z
-    double x, y, z;
-    int i=0;
-    while(3==fscanf(fp_1, "%lf, %lf, %lf\n", &x, &y, &z)){
-        cloud.points[i].x = x;
-        cloud.points[i].y = y;
-        cloud.points[i].z = z;
-        ++i;
+    number_txt = m_vTxtPoints.size();
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+
+    // Fill in the cloud data
+    cloud->width = number_txt;
+    cloud->height = 1;
+    cloud->is_dense = false;
+    cloud->points.resize(cloud->width*cloud->height);
+
+    for(int i=0; i<cloud->points.size(); ++i){
+        cloud->points[i].x = m_vTxtPoints[i].x;
+        cloud->points[i].y = m_vTxtPoints[i].y;
+        cloud->points[i].z = m_vTxtPoints[i].z;
     }
-    fclose(fp_1);
-
-    // put points cloud information to a pcd file
-    pcl::io::savePCDFileASCII("03.pcd", cloud);
-    std::cerr<<"Saved "<<cloud.points.size()<<" data points to test_pcd.pcd."<<std::endl;
-    system(pause);
+    
+    pcl::io::savePCDFileASCII("/home/epengc/Documents/3DPointCloudProject/*.pcd", *cloud);
+    std::cerr<<"Saved "<<cloud->points.size()<<" data points to txt2pcd.pcd."<<std::endl;
+    
+    // PCL Visualizer
+    pcl::visualization::PCLVisualizer viewer("Cloud Viewer");
+    viewer.addPointCloud(cloud);
+    viewer.setBackgroundColor(1, 0.5, 1);
+    viewer.spin();
+    system("pause");
 
     return 0;
 }
